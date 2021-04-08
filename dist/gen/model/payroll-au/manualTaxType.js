59a8d281ac806ab848b48851af1c672b66d65b7c
Stepan <stepan.hilbert@gmail.com>
2021-04-08 11:40:41 +0800

    build 4.11.2 with patch and un-ignore dist dir

---
+++ dist/gen/model/payroll-au/manualTaxType.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ManualTaxType = void 0;
+var ManualTaxType;
+(function (ManualTaxType) {
+    ManualTaxType[ManualTaxType["PAYGMANUAL"] = 'PAYGMANUAL'] = "PAYGMANUAL";
+    ManualTaxType[ManualTaxType["ETPOMANUAL"] = 'ETPOMANUAL'] = "ETPOMANUAL";
+    ManualTaxType[ManualTaxType["ETPRMANUAL"] = 'ETPRMANUAL'] = "ETPRMANUAL";
+    ManualTaxType[ManualTaxType["SCHEDULE5MANUAL"] = 'SCHEDULE5MANUAL'] = "SCHEDULE5MANUAL";
+    ManualTaxType[ManualTaxType["SCHEDULE5STSLMANUAL"] = 'SCHEDULE5STSLMANUAL'] = "SCHEDULE5STSLMANUAL";
+})(ManualTaxType = exports.ManualTaxType || (exports.ManualTaxType = {}));
+//# sourceMappingURL=manualTaxType.js.map
